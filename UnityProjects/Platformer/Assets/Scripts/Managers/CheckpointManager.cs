8fa909f46330b52ddac8ea31f6dbd7073d5c6b13
PauSoleT <pausoltor@gmail.com>
2022-04-06 12:05:23 +0200

    Create CheckpointManager

--- UnityProjects/Platformer/Assets/Scripts/Managers/CheckpointManager.cs
+++ UnityProjects/Platformer/Assets/Scripts/Managers/CheckpointManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CheckpointManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Checkpoint[] levelCheckpoints;
+    public Vector3 respawnPosition;
+
+    public void CheckLastCheckpoint()
     {
-        
+        foreach (Checkpoint checkpoint in levelCheckpoints)
+        {
+            if (checkpoint.playerHasReached)
+                respawnPosition = checkpoint.checkpointPosition;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CheckLastCheckpoint();
     }
 }
