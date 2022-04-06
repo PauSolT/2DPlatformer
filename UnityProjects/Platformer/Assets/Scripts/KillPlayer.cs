6cc3b4c162ce3d7be8610828afdd7aae53aeb3ac
PauSoleT <pausoltor@gmail.com>
2022-04-06 11:23:31 +0200

    Add death particles

--- UnityProjects/Platformer/Assets/Scripts/KillPlayer.cs
+++ UnityProjects/Platformer/Assets/Scripts/KillPlayer.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour
 {
+    public ParticleSystem DeathParticles;
+    ParticleSystem.MainModule DeathParticlesMain;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            Instantiate(DeathParticles, player.gameObject.transform.position, player.gameObject.transform.rotation,gameObject.transform );
+            //TODO change particles color
+            //DeathParticlesMain.startColor = playerColors[layer - minimumLayer];
             PlayerMovement.KillPlayer();
         }
     }
